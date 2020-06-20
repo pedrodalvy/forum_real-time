724079a4ee2722e988226ff872c025c4fa9e93fa
pedrodalvy <pedrodalvy@outlook.com>
2020-06-20 10:09:16 -0400

    Criado um header para a aplicação

---
+++ resources/views/layouts/default/header.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <div class="nav-wrapper">
+        <div class="container">
+            <a href="#" class="brand-logo">{{ env('APP_NAME') }}</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
