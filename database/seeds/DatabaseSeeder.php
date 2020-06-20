081faa993082174ae1268a00f8f75d0d2772b76b
pedrodalvy <pedrodalvy@outlook.com>
2020-06-19 21:38:14 -0400

    Criadas seeds para as factories

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(ThreadSeeder::class);
+        $this->call(ReplySeeder::class);
     }
 }
