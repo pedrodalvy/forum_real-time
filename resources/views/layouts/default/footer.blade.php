aeacf64357efe9907829dd66701353540ade56db
pedrodalvy <pedrodalvy@outlook.com>
2020-06-20 10:08:52 -0400

    Criado um footer para a aplicação

---
+++ resources/views/layouts/default/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="page-footer" style="padding: 10px !important;">
+    <div class="container">
+        <span class="m6">
+            © {{ date('Y') }} Copyright Pedro Dalvy
+        </span>
+    </div>
+</footer>
