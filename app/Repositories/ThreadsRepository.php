a5e1f872a506304824e34257da8495b1b584ddb7
pedrodalvy <pedrodalvy@outlook.com>
2020-06-20 18:53:36 -0400

    Criado o ThreadsRepositpry

--- app/Repositories/ThreadsRepository.php
+++ app/Repositories/ThreadsRepository.php
@@ -4,7 +4,24 @@
 namespace App\Repositories;
 
 
+use App\Models\Thread;
+
 class ThreadsRepository
 {
+    protected $threads;
 
+    public function __construct(Thread $thread)
+    {
+        $this->threads = $thread;
+    }
+
+    public function all()
+    {
+        return $this->threads::orderBy('updated_at', 'desc')->with('replies')->paginate();
+    }
+
+    public function find($id)
+    {
+        return $this->threads::findOrFail($id);
+    }
 }
