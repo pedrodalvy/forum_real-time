fc6a7bd1830ac3adf18f20a8fd5486b30e8aecf6
pedrodalvy <pedrodalvy@outlook.com>
2020-06-20 18:54:08 -0400

    Criado o método para retorno dos tópicos em json

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -3,81 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\Thread;
+use App\Repositories\ThreadsRepository;
 use Illuminate\Http\Request;
 
 class ThreadsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    protected $threadRepository;
+
+    public function __construct(ThreadsRepository $threadRepository)
+    {
+        $this->threadRepository = $threadRepository;
+    }
+
+
     public function index()
     {
-        //
+        try {
+            $threads = $this->threadRepository->all();
+            return response()->json($threads);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Thread $thread)
+
+    public function show($id)
     {
-        //
+        try {
+            $thread = $this->threadRepository->find($id);
+            return view('threads.view', compact('thread'));
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Thread $thread)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Thread $thread)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Thread $thread)
     {
         //
