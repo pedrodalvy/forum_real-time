cd3a15874db7ba7f61ff12780ce7103ebcc66e7e
pedrodalvy <pedrodalvy@outlook.com>
2020-06-20 18:54:43 -0400

    Adicionado relacionamento e links de visualização no model Thread

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -6,7 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    protected $appends = ['links'];
+
     protected $fillable = [
         'body', 'thread_id', 'user_id'
     ];
+
+    public function getLinksAttribute()
+    {
+        return [
+            'href' => route('threads.show', $this->id),
+            'rel' => 'Exibir Tópico'
+        ];
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
