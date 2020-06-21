8b2aca3c2991d5272799fa84479b61c1134ad312
pedrodalvy <pedrodalvy@outlook.com>
2020-06-21 15:28:56 -0400

    Criado ThreadTest para testes dos tópicos

--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -2,21 +2,41 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Thread;
+use App\Repositories\ThreadsRepository;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
 class ThreadTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
 
-        $response->assertStatus(200);
+    public function testActionIndexOnController()
+    {
+        $user = factory(\App\Models\User::class)->create();
+        $this->seed('ThreadSeeder');
+        $threads = (new ThreadsRepository(new Thread))->all();
+
+        $this->actingAs($user)
+            ->json('GET', '/threads')
+            ->assertStatus(200)
+            ->assertJsonFragment([$threads->toArray()['data']]);
+    }
+
+
+    public function testActionStoreOnController()
+    {
+        $user = factory(\App\Models\User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->json('POST', '/threads', [
+                'title' => 'Never fire a scallywag',
+                'body' => 'Glutens sunt bromiums de castus nutrix',
+            ]);
+        $thread = (new ThreadsRepository(new Thread))->find(1);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment(['created' => 'success'])
+            ->assertJsonFragment(['data' => $thread->toArray()]);
     }
 }
