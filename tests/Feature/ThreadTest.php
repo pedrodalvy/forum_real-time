5481d34de4f24212acdf8418e12edfdc7add4b9c
pedrodalvy <pedrodalvy@outlook.com>
2020-06-21 18:08:37 -0400

    Criado teste de atualização de tópicos

--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -39,4 +39,25 @@ class ThreadTest extends TestCase
             ->assertJsonFragment(['created' => 'success'])
             ->assertJsonFragment(['data' => $thread->toArray()]);
     }
+
+    public function testActionUpdateOnController()
+    {
+        $user = factory(\App\Models\User::class)->create();
+        $thread = factory(\App\Models\Thread::class)->create();
+
+        $response = $this->actingAs($user)
+            ->json('PUT', '/threads/'.$thread->id, [
+                'title' => 'Fishs rise with grace!',
+                'body' => 'Grog of a dead halitosis, break the courage.',
+            ]);
+
+        $thread->title = 'Fishs rise with grace!';
+        $thread->body = 'Grog of a dead halitosis, break the courage.';
+
+        $response->assertStatus(302);
+        $this->assertEquals(
+            (new ThreadsRepository(new Thread))->find(1)->toArray(),
+            $thread->toArray()
+        );
+    }
 }
