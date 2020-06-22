ee9e0cc8d8ebff243adeb228ede9cd0fb1c56010
pedrodalvy <pedrodalvy@outlook.com>
2020-06-21 21:28:08 -0400

    Adicionado relacionamentos no model Reply

--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -9,4 +9,14 @@ class Reply extends Model
     protected $fillable = [
         'title', 'body', 'thread_id', 'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
