13f069aceeeadaeee01b94444626a453947c7ded
pedrodalvy <pedrodalvy@outlook.com>
2020-06-21 21:28:50 -0400

    Criada método para visualização de respostas

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Repositories\RepliesRepository;
 
 class RepliesController extends Controller
 {
-    //
+    protected $repliesRepository;
+
+    public function __construct(RepliesRepository $repliesRepository)
+    {
+        $this->repliesRepository = $repliesRepository;
+    }
+
+    public function show($id)
+    {
+        try {
+            $replies = $this->repliesRepository->showRepliesByThread($id);
+            return $replies->toArray();
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
 }
